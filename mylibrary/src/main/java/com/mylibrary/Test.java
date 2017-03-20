8ced7680b777d0f7fbdabce2660745bfd8cee614
Suprada <suprada.rao>
2017-03-20 19:00:33 +0530

    latest

--- mylibrary/src/main/java/com/mylibrary/Test.java
+++ mylibrary/src/main/java/com/mylibrary/Test.java
@@ -7,9 +7,8 @@ import android.widget.Toast;
  * Created by Suprada on 20-Mar-17.
  */
 
-public class Test {
-    public void PrintMsg(Context c, String x)
-    {
+class TestClass {
+    public void PrintMsg(Context c, String x) {
         Toast.makeText(c, x, Toast.LENGTH_SHORT).show();
     }
 }
